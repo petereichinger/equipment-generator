6dde2879af9b2b4fb57950ce31f71a3512dba52e
Peter Eichinger <peter.eichinger@gmail.com>
2016-07-04 11:28:10 +0200

    Add math class with alternative Mod implementation

---
+++ Assets/Scripts/Math.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace EquipmentGenerator {
+
+	public static class Math {
+
+		/// <summary>
+		/// Calculate x mod m. Correctly handles negative values. Resuling value is always in the range [0;m-1]
+		/// </summary>
+		/// <param name="x">Value</param>
+		/// <param name="m">Modus</param>
+		/// <returns>Modulus m of x. Value between 0 and m-1.</returns>
+		public static int Mod(int x, int m) {
+			int r = x % m;
+			return r < 0 ? r + m : r;
+		}
+	}
+}
