ae5cdea6a6ac30bf8d9a3fe9ab0a06ec5b1d5586
Peter Eichinger <peter.eichinger@gmail.com>
2016-07-04 11:32:35 +0200

    Add random size for round shield

--- Assets/Scripts/Shield/ShieldGenerator.cs
+++ Assets/Scripts/Shield/ShieldGenerator.cs
@@ -6,15 +6,18 @@ namespace EquipmentGenerator.Shield {
 
 	public class ShieldGenerator : IGenerator {
 		public int roundVertices = 50;
+		public float minRadius = .75f;
+		public float maxRadius = 1.5f;
 
 		public Mesh Generate(int seed) {
 			Random.seed = seed;
 			var mesh = new Mesh { name = seed.ToString() };
+			float size = Random.Range(minRadius, maxRadius);
 			Vector3[] vertices = new Vector3[roundVertices + 1];
 			vertices[0] = new Vector3(0, 0, 0);
 			for (int i = 0; i < roundVertices; i++) {
 				float angle = 2 * Mathf.PI * (i / (float)roundVertices);
-				vertices[i + 1] = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0);
+				vertices[i + 1] = new Vector3(size * Mathf.Cos(angle), size * Mathf.Sin(angle), 0);
 			}
 			int[] triIndices = new int[roundVertices * 3];
 
