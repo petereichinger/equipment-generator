80c0ea966d0eb30ad091fb29e099fc6ad7ab2e14
Peter Eichinger <peter.eichinger@gmail.com>
2016-07-04 11:30:22 +0200

    Get mesh from shield generator and apply it to filter

--- Assets/Scripts/Shield/ShieldGeneratorComponent.cs
+++ Assets/Scripts/Shield/ShieldGeneratorComponent.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 
 namespace EquipmentGenerator.Shield {
 
+	[RequireComponent(typeof(MeshFilter))]
+	[RequireComponent(typeof(MeshRenderer))]
 	public class ShieldGeneratorComponent : MonoBehaviour {
 
 		public void GenerateShield() {
+			var newShield = new ShieldGenerator().Generate(System.Environment.TickCount);
+			var meshFilter = GetComponent<MeshFilter>();
+			DestroyImmediate(meshFilter.sharedMesh);
+			meshFilter.sharedMesh = newShield;
 		}
 	}
 }
