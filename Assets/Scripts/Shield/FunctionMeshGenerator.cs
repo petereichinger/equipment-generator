f6362ce805bd9eae1a6140d2441b996b5d1668dd
Peter Eichinger <peter.eichinger@gmail.com>
2016-07-05 16:34:20 +0200

    Only add vertices at y==0 and y==function value

--- Assets/Scripts/Shield/FunctionMeshGenerator.cs
+++ Assets/Scripts/Shield/FunctionMeshGenerator.cs
@@ -3,69 +3,37 @@ using UnityEngine;
 
 public class FunctionMeshGenerator {
 
-	public Mesh Generate(System.Func<float, float> function, int resX, int resY, bool invert = false) {
+	public Mesh Generate(System.Func<float, float> function, int resolution) {
 		var mesh = new Mesh();
 
-		float xStepSize = 1f / resX;
-		float yStepSize = 1f / resY;
-
-		List<Vector3> points = new List<Vector3>();
+		List<Vector3> points = new List<Vector3>(resolution * 2 + 1);
 
 		List<int> triangleIndices = new List<int>();
 
-		List<Vector3> newPoints = new List<Vector3>(resY + 1);
-		List<Vector3> oldPoints = new List<Vector3>(resY + 1);
-		for (int i = 0; i <= resX; i++) {
-			float x = (float)i / resX;
+		List<Vector3> newPoints = new List<Vector3>(3);
+		List<Vector3> oldPoints = new List<Vector3>(3);
+		for (int i = 0; i <= resolution; i++) {
+			float x = (float)i / resolution;
 			float y = function(x);
 
 			newPoints.Clear();
-			if (invert) {
-				for (float yGrid = y; yGrid < 1; yGrid += yStepSize) {
-					newPoints.Add(new Vector3(x, yGrid));
-				}
-				newPoints.Add(new Vector3(x, 1));
-			} else {
-				for (float yGrid = 0; yGrid < y; yGrid += yStepSize) {
-					newPoints.Add(new Vector3(x, yGrid));
-				}
+			newPoints.Add(new Vector3(x, 0));
+			if (y > 0f) {
 				newPoints.Add(new Vector3(x, y));
 			}
 
+			int offsetOld = points.Count;
+			int offsetNew = offsetOld + oldPoints.Count;
+
 			if (oldPoints.Count > 0) {
-				int offsetOld = points.Count;
-				int offsetNew = offsetOld + oldPoints.Count;
-				int minCount = Mathf.Min(oldPoints.Count, newPoints.Count) - 1;
-				int maxCount = Mathf.Max(oldPoints.Count, newPoints.Count);
-				int index = 0;
-
-				while (index < minCount) {
-					triangleIndices.Add(offsetOld + index);
-					triangleIndices.Add(offsetOld + index + 1);
-					triangleIndices.Add(offsetNew + index + 1);
-
-					triangleIndices.Add(offsetOld + index);
-					triangleIndices.Add(offsetNew + index + 1);
-					triangleIndices.Add(offsetNew + index);
-
-					index++;
+				if (oldPoints.Count == 2) {
+					triangleIndices.Add(offsetOld);
+					triangleIndices.Add(offsetOld + 1);
+					triangleIndices.Add(offsetNew + 1);
 				}
-
-				if (oldPoints.Count < newPoints.Count) {
-					while (index < newPoints.Count - 1) {
-						triangleIndices.Add(offsetNew - 1);
-						triangleIndices.Add(offsetNew + index + 1);
-						triangleIndices.Add(offsetNew + index);
-						index++;
-					}
-				} else if (newPoints.Count < oldPoints.Count) {
-					while (index < oldPoints.Count - 1) {
-						triangleIndices.Add(offsetOld + index);
-						triangleIndices.Add(offsetOld + index + 1);
-						triangleIndices.Add(offsetNew + newPoints.Count - 1);
-						index++;
-					}
-				}
+				triangleIndices.Add(offsetOld);
+				triangleIndices.Add(offsetNew + 1);
+				triangleIndices.Add(offsetNew);
 			}
 
 			points.AddRange(oldPoints);
