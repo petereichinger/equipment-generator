ae9cbc4afb7e36f1278647ebae254213fa619d0f
Peter Eichinger <peter.eichinger@gmail.com>
2016-07-10 16:49:20 +0200

    Add depth parameter and generate a second mesh on the backside

--- Assets/Scripts/Shield/ShieldMeshGenerator.cs
+++ Assets/Scripts/Shield/ShieldMeshGenerator.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class ShieldMeshGenerator {
 
 	public static SubMesh GenerateShield(System.Func<float, float> upperFunction, System.Func<float, float> lowerFunction, IOverlayShape overlayShape,
-		int resolution,
+		int resolution, float depth,
 		float leftMargin = -0.5f, float rightMargin = 0.5f) {
 		var verts = new List<Vector3>();
 		var tris = new List<int>();
@@ -29,33 +29,41 @@ public class ShieldMeshGenerator {
 				newValues.Value2 = new Vector2(value, upperY);
 			}
 			int oldOffset = verts.Count;
-			int newOffset = oldOffset + Tuple.NullableHasValueCount(oldValues);
+			int newOffset = oldOffset + 2 * Tuple.NullableHasValueCount(oldValues);
 			if (oldValues.Value1.HasValue) {
 				if (oldValues.Value2.HasValue) {
 					// Both old values are set.
-					tris.AddTriangle(oldOffset, oldOffset + 1, newOffset);
 
 					if (newValues.Value2.HasValue) {
 						// New values has two values as well
+						tris.AddTriangle(oldOffset, oldOffset + 1, newOffset);
 						tris.AddTriangle(oldOffset + 1, newOffset + 1, newOffset);
+
+						tris.AddTriangle(oldOffset + 2, oldOffset + 3, newOffset + 2, true);
+						tris.AddTriangle(oldOffset + 3, newOffset + 3, newOffset + 2, true);
+					} else {
+						// New values only has one value
+						tris.AddTriangle(oldOffset, oldOffset + 1, newOffset);
+						tris.AddTriangle(oldOffset + 2, oldOffset + 3, newOffset + 1, true);
 					}
 				} else {
 					if (newValues.Value2.HasValue) {
 						// New values has two values, old values just one
 						tris.AddTriangle(oldOffset, newOffset + 1, newOffset);
+						tris.AddTriangle(oldOffset + 1, newOffset + 3, newOffset + 2, true);
 					}
 				}
 			}
 
 			verts.AddPointTuple(oldValues);
-
+			verts.AddPointTuple(oldValues, depth);
 			oldValues.CopyFrom(newValues);
 			newValues.Assign();
 			value += step;
 		}
 
 		verts.AddPointTuple(oldValues);
-
+		verts.AddPointTuple(oldValues, depth);
 		overlayShape.Overlay(verts);
 		return new SubMesh(verts, tris);
 	}
