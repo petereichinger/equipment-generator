491bbe63229c568cd84d4a97c8a776c5e0f41d53
Peter Eichinger <peter.eichinger@gmail.com>
2016-07-10 16:08:35 +0200

    Default constructor for tuple and CopyFrom() method as well as XML-comments

--- Assets/Scripts/Tuple.cs
+++ Assets/Scripts/Tuple.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Text;
 
 /// <summary>Class representing a generic tuple.</summary>
 /// <typeparam name="T1">Type of the first value in the tuple. See <see cref="Value1"/>.</typeparam>
@@ -12,14 +13,37 @@ public class Tuple<T1, T2> {
 	/// <summary>Second value in the tuple.</summary>
 	public T2 Value2 { get; set; }
 
+	/// <summary>
+	/// Create a new Tuple with the default values of <typeparamref name="T1"/> and <typeparamref name="T2"/>.
+	/// </summary>
+	public Tuple() {
+		Value1 = default(T1);
+		Value2 = default(T2);
+	}
+
+	/// <summary>
+	/// Create a new Tuple with the specified values for <see cref="Value1"/> and <see cref="Value2"/>.
+	/// </summary>
+	/// <param name="val1">First value.</param>
+	/// <param name="val2">Second value.</param>
 	public Tuple(T1 val1, T2 val2) {
 		Value1 = val1;
 		Value2 = val2;
 	}
 
-	public void Clear(T1 val1 = default(T1), T2 val2 = default(T2)) {
+	/// <summary>Assign values for <see cref="Value1"/> and <see cref="Value2"/>.</summary>
+	/// <param name="val1">Value for <see cref="Value1"/>. Default is <c>default(T1)</c>.</param>
+	/// <param name="val2">Value for <see cref="Value2"/>. Default is <c>default(T2)</c>.</param>
+	public void Assign(T1 val1 = default(T1), T2 val2 = default(T2)) {
 		Value1 = default(T1);
 		Value2 = default(T2);
+	}
+
+	/// <summary>Copy the values from another Tuple.</summary>
+	/// <param name="other">The other tuple.</param>
+	public void CopyFrom(Tuple<T1, T2> other) {
+		Value1 = other.Value1;
+		Value2 = other.Value2;
 	}
 }
 
@@ -47,6 +71,26 @@ public class Tuple<T1, T2, T3> {
 
 /// <summary>Helper class for creating tuples. Just use <c>Tuple.Create(...)</c>.</summary>
 public static class Tuple {
+
+	/// <summary>
+	/// Get a number representing the number of values that are not null in <paramref name="tuple"/>. <c>0</c> if both
+	/// <see cref="Tuple{T1,T2}.Value1"/> and <see cref="Tuple{T1,T2}.Value2"/> have no value, <c>1</c> if either <see
+	/// cref="Tuple{T1,T2}.Value1"/> or <see cref="Tuple{T1,T2}.Value2"/> have a value, <c>2</c> otherwise.
+	/// </summary>
+	/// <typeparam name="T1">Type of <see cref="Tuple{T1,T2}.Value1"/>. Must be a <c>struct</c>.</typeparam>
+	/// <typeparam name="T2">Type of <see cref="Tuple{T1,T2}.Value2"/>. Must be a <c>struct</c>.</typeparam>
+	/// <param name="tuple">Tuple to count.</param>
+	/// <returns>
+	/// <c>0</c> if both <see cref="Tuple{T1,T2}.Value1"/> and <see cref="Tuple{T1,T2}.Value2"/> have no value, <c>1</c>
+	/// if either <see cref="Tuple{T1,T2}.Value1"/> or <see cref="Tuple{T1,T2}.Value2"/> have a value, <c>2</c>
+	/// otherwise.
+	/// </returns>
+	public static int NullableHasValueCount<T1, T2>(Tuple<T1?, T2?> tuple)
+		where T1 : struct
+		where T2 : struct {
+		return (tuple.Value1.HasValue ? 1 : 0) +
+			(tuple.Value2.HasValue ? 1 : 0);
+	}
 
 	public static Tuple<T1?, T1> CreateNullValue<T1>(T1 val1) where T1 : struct {
 		return new Tuple<T1?, T1>(null, val1);
