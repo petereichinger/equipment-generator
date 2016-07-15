d8297708a4b8bf432f418b1d728be55cd90f8dae
Peter Eichinger <peter.eichinger@gmail.com>
2016-07-15 15:43:29 +0200

    Show vertex positions and set length of normals

--- Assets/Scripts/MeshDebug.cs
+++ Assets/Scripts/MeshDebug.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 [ExecuteInEditMode]
 [RequireComponent(typeof(MeshFilter))]
 public class MeshDebug : MonoBehaviour {
+	public float length = 0.1f;
+
+	public bool showVertices = false;
 
 	private void Update() {
 		var mesh = GetComponent<MeshFilter>().sharedMesh;
+		var origin = transform.TransformPoint(Vector3.zero);
 		for (int i = 0; i < mesh.vertexCount; i++) {
 			var pos = transform.TransformPoint(mesh.vertices[i]);
 
-			Debug.DrawRay(pos, transform.TransformDirection(mesh.normals[i]) * 0.1f, Color.red, 10f);
+			Debug.DrawRay(pos, transform.TransformDirection(mesh.normals[i]) * length, Color.red, 10f);
+
+			if (showVertices) {
+				Debug.DrawLine(origin, pos, Color.blue);
+			}
 		}
 	}
 }
